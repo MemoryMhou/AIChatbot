5b88d01d50a788a8985c7fcff5edf2e0c8dd02df
MemoryMhou <mhoumemory1@gmail.com>
2023-05-24 23:00:07 +0200

    Initial Commit

---
+++ AIChatbot/AIChatbotApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatbotApp.swift
+//  AIChatbot
+//
+//  Created by Memory Mhou on 24/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct AIChatbotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
